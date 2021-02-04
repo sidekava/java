2d9ef02b089bea9586cec798b73256d2e8f78831
sidekava <sidekava@gmail.com>
2021-02-04 23:01:18 +0900

    자바 객체 (재)7, 8, 9강 getter와 setter를 사용하는 이유 코드 분리

---
+++ StructuredJavaPrj2/src/part3/ex3/Getters와Setters/Exam.java
@@ -0,0 +1,48 @@
+package part3.ex3.Getters와Setters;
+
+public class Exam {
+	int kor;
+	int eng;
+	int math;
+	
+	public Exam() {
+		this(0, 0, 0);
+	}
+	
+	public Exam(int kor, int eng, int math) {
+		//this의 변수보다 지역변수가 우선순위가 높다. 그래서 this를 생략하면 파라미터로 받은 kor에 값이 들어간다.
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public float avg() {
+		// TODO Auto-generated method stub
+		return total()/3.0f;
+		
+	}
+	public int total() {
+		// TODO Auto-generated method stub
+		return kor + eng + math;
+	}
+	
+	
+
+}
--- StructuredJavaPrj2/src/part3/ex3/Getters와Setters/ExamList.java
+++ StructuredJavaPrj2/src/part3/ex3/Getters와Setters/ExamList.java
@@ -1,4 +1,4 @@
-package part3.ex2.메소드;
+package part3.ex3.Getters와Setters;
 
 import java.util.Scanner;
 
@@ -20,12 +20,12 @@ public class ExamList {
 		
 		for(int i = 0; i < size; i++) {
 			Exam exam = exams[i];
-			int kor = exam.kor;
-			int eng = exam.eng;
-			int math= exam.math;
+			int kor = exam.getKor();
+			int eng = exam.getEng();
+			int math= exam.getMath();
 			
-			int total = kor + eng + math;
-			float avg = total/3.0f;
+			int total = exam.total();//kor + eng + math;
+			float avg = exam.avg();//total/3.0f;
 			
 			System.out.printf("국어 : %3d\n", exam.kor);	        	
 			System.out.printf("영어 : %3d\n", exam.eng);	        	
@@ -75,11 +75,12 @@ public class ExamList {
         	
         } while(math < 0 || 100 < math);
         
-        Exam exam = new Exam();
-        exam.kor = kor;
-        exam.eng = eng;
-        exam.math = math;
-        
+/*      Exam exam = new Exam();
+        exam.setKor(kor);
+        exam.setEng(eng);
+        exam.setMath(math);
+*/        
+        Exam exam = new Exam(kor, eng, math);
         Exam[] exams = this.exams;
         int size = this.current;
         
