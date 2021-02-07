5606ee0f68aba8484b501537d75cc83054d43d8d
sidekava <sidekava@gmail.com>
2021-02-07 21:39:01 +0900

    자바 객체 (재)10강 UI코드 분리, 분리하는 이유는 코드 재사용을 위해

---
+++ StructuredJavaPrj2/src/part3/ex4/UI코드분리하기/Program.java
@@ -0,0 +1,54 @@
+package part3.ex4.UI코드분리하기;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		ExamList list = new ExamList();
+		
+		int menu;
+        boolean keepLoop = true;			
+		
+		
+		while(keepLoop)
+		{
+
+			menu = inputMenu();
+	        
+	        switch(menu) {	        
+	        case 1:
+//	        	ExamList.inputList(list);
+	        	list.inputList();
+		        break;
+	        case 2:
+//	        	ExamList.printList(list);
+	        	list.printList();
+		        break;
+	        case 3:
+	        	System.out.println("Bye~~");
+	        	
+	        	keepLoop = false;
+	        	break;
+	        default:
+	        	System.out.println("잘못된 값을 입력하셨습니다. 메뉴는 1~3까지입니다.");
+	       
+	        }
+		}   
+	}
+	
+	static int inputMenu() {
+        Scanner scan = new Scanner(System.in);
+		System.out.println("┌───────────────────────────┐");
+        System.out.println("│           메인 메뉴                   │");
+        System.out.println("└───────────────────────────┘");
+        System.out.println("\t1. 성적입력 ");
+        System.out.println("\t2. 성적출력 ");
+        System.out.println("\t3. 종료 ");
+        System.out.print("\t선택> ");
+        int menu = scan.nextInt(); 
+        
+        return menu;
+    }
+
+}
