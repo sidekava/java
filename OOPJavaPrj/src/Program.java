9520b43b086bbd3f4cae34e18f180ab7d79717af
sidekava <sidekava@gmail.com>
2021-02-21 22:05:07 +0900

    자바 객체 13강 코드 재사용 코드 재사용은 소스 코드를 재사용하는 것이 아닌 바이트 코드를 재사용한다. 재사용할 코드를 jar로 만들어 사용하고자 하는 프로젝트의 library에 추가하여 사용.

---
+++ OOPJavaPrj/src/Program.java
@@ -0,0 +1,17 @@
+import part3.ex4.UI코드분리하기.Exam;
+
+public class Program {
+	
+	// 1. 컴파일 -> Exam.class
+	// 2. 압축 -> Exam.zip
+	// 3. jar 만들기 -> Exam.jar
+	// 위 과정은 이클립스가 지원함.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exam exam = new Exam(1, 1, 1);
+		
+		System.out.println(exam.total());
+	}
+
+}
