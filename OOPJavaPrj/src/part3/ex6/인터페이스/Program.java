101e3c76fadc01c96480ae9b1718cdf72721813c
sidekava <sidekava@gmail.com>
2021-03-13 22:32:51 +0900

    자바 객체 33강 인터페이스 객체를 외부파일 통해 바꾸기

--- OOPJavaPrj/src/part3/ex6/인터페이스/Program.java
+++ OOPJavaPrj/src/part3/ex6/인터페이스/Program.java
@@ -1,11 +1,25 @@
 package part3.ex6.인터페이스;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException {
+		FileInputStream fis = new FileInputStream("src/part3/ex6/인터페이스/setting.txt");
+		Scanner scan = new Scanner(fis);
+		String className = scan.nextLine();
+		scan.close();
+		fis.close();
+		
+		
+		Class clazz = Class.forName(className);
 		A a = new A();
-		B b = new B();
-		a.setX(b);
+		X x = (X) clazz.getDeclaredConstructor().newInstance();
+		a.setX(x);
 		a.print();
 	}
 
