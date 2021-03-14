83bf93896b175e7c7812d3d3249a44fca585f3cd
sidekava <sidekava@gmail.com>
2021-03-14 22:02:43 +0900

    자바 객체 34강 인터페이스 구현 및 일부 기능 분리

---
+++ GamePrj/src/gamePrj/GameWindowListener.java
@@ -0,0 +1,46 @@
+package gamePrj;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JOptionPane;
+
+public class GameWindowListener implements WindowListener {
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		JOptionPane.showMessageDialog(null, "Good bye~~");
+		System.exit(0);
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		
+	}
+
+}
